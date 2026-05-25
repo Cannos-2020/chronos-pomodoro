50deff4355a5c227ed0dd0e9a9b451fd35dc31ee
Bruno Cannos <brunocannos2020@gmail.com>
2026-05-25 17:51:16 -0300

    Commit update geral

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+
 import './styles/theme.css'
 import './styles/global.css'
-import { Container } from './components/Container';
-import { Heading } from './components/Heading';
-
-
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { PlayCircleIcon } from 'lucide-react';
+import { DefaultButton } from './components/DefaultButton';
 
 export function App() {
 
     return (
     <>
         <Container>
-            <Heading>LOGO</Heading>
+            <Logo/>
         </Container>
 
         <Container>
-            <Heading>MENU</Heading>
+            <Menu/>
+        </Container>
+
+        <Container>
+            <CountDown/>
+        </Container>
+
+        <Container>
+            <form className='form' action="">
+                <div className='formRow'>
+                    <DefaultInput  
+                     labelText='task' 
+                     id='meuInput' 
+                     type='text' 
+                     placeholder='Digite algo'
+                    />
+                </div>
+
+                <div className='formRow'>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+
+                <div className='formRow'>
+                    <Cycles/>
+                </div>
+
+                <div className='formRow'>
+                    <DefaultButton icon={<PlayCircleIcon/>}/>
+                </div>
+            </form>
         </Container>
     </>
     );
